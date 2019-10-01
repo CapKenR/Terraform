84c55f897cb701e3607f86771cec9a8cc60364ee
Ken Rider <ken.rider@capstonec.com>
2019-10-01 12:36:57 -0600

    Add vSphere provider test example.

---
+++ Examples/vsphere-provider-test-variables.tf
@@ -0,0 +1,11 @@
+variable "vsphere_server" {
+  description = "The hostname of the vCenter to use."
+}
+
+variable "vsphere_user" {
+  description = "The username to use when connecting to vCenter."
+}
+
+variable "vsphere_password" {
+  description = "The password to use when connecting to vCenter."
+}
